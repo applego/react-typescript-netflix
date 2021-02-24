4a7ed873b2f2d384979b19835415e1a519b0379a
applego <applegorillappa@gmail.com>
2021-02-25 00:34:19 +0900

    use genre from json

--- src/request.js
+++ src/request.js
@@ -1,12 +1,15 @@
 import { API_KEY } from './secrets';
+import { jsonGenres } from './secrets_genre';
+
+const genres = jsonGenres.genres;
 
 export const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
   fetchTopRated: `/discover/tv?api_key=${API_KEY}&languager=en-us`,
-  fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+  fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=${genres.Action.id}`,
+  fetchComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=${genres.Comedy.id}`,
+  fetchHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=${genres.Horror.id}`,
+  fetchRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=${genres.Romance.id}`,
+  fetchDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=${genres.Documentary.id}`,
 };
